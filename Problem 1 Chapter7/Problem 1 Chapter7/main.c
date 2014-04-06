f07540860465fcc49588a0b63d9b8b5a53496e5f
Briguy2 <briansullivan2@gmail.com>
2014-04-05 21:40:28 -0500

    This is the first problem of the program problem level 2 to be turned in for credit.

---
+++ Problem 1 Chapter7/Problem 1 Chapter7/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Problem 1 Chapter7
+//
+//  Created by Programming on 3/18/14.
+//  Copyright (c) 2014 Brian Sullivan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
