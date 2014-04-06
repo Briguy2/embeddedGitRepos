eaaafed904aad198212eae20c5a4d855c688cfcd
Briguy2 <briansullivan2@gmail.com>
2014-04-05 22:17:57 -0500

    uptdating this repo to include the actual answer for level 2 problem 1 with file titled Problem1_1.c. also I'm including an older file called serchForSpecialChar_array which may do the same thing as the answer to problem 1 from level 2

---
+++ Problem1_1.c
@@ -0,0 +1,33 @@
+/* 
+Brian Sullivan
+Embedded Programming 2014
+Program Level 2
+	Problem 1
+	this code should return the number of how 
+	many of a selected charecter exists within a
+	phrase that is entered by the user. 
+	   This will be accomplished by:
+	creating an array[ ] that
+	"scanf" or "gets" will be used to enter the user's 
+	phrase into, and then possably another function to search the array
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	char array[80], search;
+	int x = 0;//while loop counter
+	int count = 0;//array element counter
+	printf("\nEnter a phrase\n");
+	gets(array);//Trying out gets() insted of scanf().
+	printf("enter the char to look for \n");
+	scanf(" %c", &search);//entering the char to look for in variable "search".
+	while (array[x] != NULL) {//If we are not at the end of the string execute the if().
+		if(array[x] == search){//If the contents of the array element is equal to the variable shearch.
+			count++;//variable "counter" is incrimented by one.
+					}
+		x++;//while() counter.
+	}
+	printf(" %c was in the phrase %d times", search, count);//printing the results in an understanable format.
+
+}
